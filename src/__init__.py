66bc79c15a3b60639da9023b217d9d616695a2ba
Calvin Feng <calvin.j.feng@gmail.com>
2022-01-21 14:40:03 -0800

    Setup Python dist configurations

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+"""Model serving provides HTTP and gRPC wrapper for model serving.
+"""
+from .http_client import *
---
+++ src/http_client.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+class HTTPClient:
+    def __init__(self):
+        self.random = np.random.randn(1, 2, 3)
